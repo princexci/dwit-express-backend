8378d6630cfff75fae94ec43d5c3a188a0368ffa
zenishshr <zenishshr@gmail.com>
2021-10-06 15:52:26 +0545

    Add cors for production site.

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.use(
       "http://localhost:3000",
       "http://127.0.0.1:3000",
       "http://localhost:3001",
+      "https://dwit-ecommerce.netlify.app",
     ], // Frontend..
     credentials: true,
   })
